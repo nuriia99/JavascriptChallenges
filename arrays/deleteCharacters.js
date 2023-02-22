fe53ce4729762c800e2fb7ecb8f970faf39ec3dc
Nuria Antón Calle <nurianton1999@gmail.com>
2023-02-22 12:23:07 +0100

    add toLowerCase

--- arrays/deleteCharacters.js
+++ arrays/deleteCharacters.js
@@ -6,8 +6,8 @@
 const splitArray = function (str1, str2) {
   let out1 = str1.toLowerCase().split('')
   let out2 = str2.toLowerCase().split('')
-  out1 = out1.filter(c => !str2.includes(c))
-  out2 = out2.filter(c => !str1.includes(c))
+  out1 = out1.filter(c => !str2.toLowerCase().includes(c))
+  out2 = out2.filter(c => !str1.toLowerCase().includes(c))
   console.log(out1, out2)
 }
 
